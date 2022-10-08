b43afd54e1f726e34f660ae528ef02a0c509bcb1
Francesco Rifugio <francesco.rifugio@avanade.com>
2022-10-08 19:26:35 +0200

    Fix Program.cs

--- KitchenStock/Program.cs
+++ KitchenStock/Program.cs
@@ -10,15 +10,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
-//if (builder.HostEnvironment.IsDevelopment())
-//{
-//    builder.Services.AddScoped<IStockRepository, MockStockRepository>();
-//}
-//else
-//{
-//    builder.Services.AddScoped<IStockRepository, StockRepository>();
-//}
+if (builder.HostEnvironment.IsDevelopment())
+{
+    builder.Services.AddScoped<IStockRepository, MockStockRepository>();
+}
+else
+{
+    builder.Services.AddScoped<IStockRepository, StockRepository>();
+}
 
-builder.Services.AddScoped<IStockRepository, StockRepository>();
 
 await builder.Build().RunAsync();
